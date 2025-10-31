c92d44e02753e3682cb0c8c30dec4dad2095d11b
frkan1079 <furkankaya118@gmail.com>
2025-10-31 15:47:23 +0300

    kayıt tarihi filtre

--- Models/ViewModels/OgrencilerFilterViewModel.cs
+++ Models/ViewModels/OgrencilerFilterViewModel.cs
@@ -4,20 +4,22 @@ namespace StudentApp.Models.ViewModels
     {
         public IEnumerable<Ogrenciler> Ogrenciler { get; set; } = new List<Ogrenciler>();
    
-   // Filtreleme parametreleri
-   public string? SearchTerm { get; set; }
+        // Filtreleme parametreleri
+    public string? SearchTerm { get; set; }
         public long? CinsiyetId { get; set; }
-    public long? OdemePlanlariId { get; set; }
+        public long? OdemePlanlariId { get; set; }
         public int? MinYas { get; set; }
         public int? MaxYas { get; set; }
-      public bool ShowPasif { get; set; } = false;
+        public DateTime? BaslangicKayitTarihi { get; set; }
+        public DateTime? BitisKayitTarihi { get; set; }
+  public bool ShowPasif { get; set; } = false;
      
         // Sýralama parametreleri
-      public string SortBy { get; set; } = "OgrenciSoyadi";
-    public string SortOrder { get; set; } = "asc";
+        public string SortBy { get; set; } = "OgrenciSoyadi";
+        public string SortOrder { get; set; } = "asc";
     
         // Dropdown listeleri
-    public IEnumerable<Cinsiyetler>? Cinsiyetler { get; set; }
+        public IEnumerable<Cinsiyetler>? Cinsiyetler { get; set; }
         public IEnumerable<OdemePlanlari>? OdemePlanlari { get; set; }
- }
+    }
 }
